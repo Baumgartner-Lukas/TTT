a2272d9f2dabbc0815aded9c441d7396eb101d87
Baumgartner_Lukas <baumgartner.webtree@gmail.com>
2018-02-28 10:04:52 +0100

    Added doc comments

--- TicTacToe/src/controller/SimpleAI.java
+++ TicTacToe/src/controller/SimpleAI.java
@@ -11,7 +11,10 @@ public class SimpleAI {
     public SimpleAI() {
     }
 
-    //get the state of the gameboard
+    /**
+     * public method to use in the GameController class
+     * @param model model of the current game board
+     */
     protected void updateGameBoard(GameBoard model) {
         alwaysDraw(model);
         counter++;
@@ -19,7 +22,6 @@ public class SimpleAI {
 
     /**
      * Adds stones randomly on the field. Easiest difficulty.
-     *
      * @param model model of the current game board
      */
     private void addRandomStone(GameBoard model) {
@@ -34,7 +36,6 @@ public class SimpleAI {
 
     /**
      * Adds stones in a way to the board, that should always lead to a draw
-     *
      * @param model model of the current game board
      */
     private void alwaysDraw(GameBoard model) {
@@ -59,6 +60,10 @@ public class SimpleAI {
         }
     }
 
+    /**
+     * checks if there is a free space on any of the middle lanes(0:1, 2:1, 1:0, 1:2)
+     * @param model current model of the game board
+     */
     private void checkStraights(GameBoard model) {
         int r = getRandomNumber();
         int c = getRandomNumber();
@@ -70,6 +75,12 @@ public class SimpleAI {
 
     }
 
+    /**
+     * checks if any of the corners of the game board is free to set a stone
+     * @param model current model of the game board
+     * @return  true if there was a free corner and a friendly stone was set
+     *          false if no corner was empty
+     */
     private boolean checkCorners(GameBoard model) {
         int cornerCount = 0;
         for (int r = 0; r < 2; r++) {
@@ -86,6 +97,13 @@ public class SimpleAI {
         return false;
     }
 
+    /**
+     * Checks if there are two enemy stones already in a diagonal position. If so, make the according counter move
+     * If there is no enemy stone in the middle, skip that check.
+     * @param model model of the current game board
+     * @return  false if there is no enemy stone in the middle.
+     *          true if there are two enemy stones in a diagonal pos and a counter move was made.
+     */
     private boolean checkDiagonal(GameBoard model) {
         if (model.getStone(1, 1) != Stone.X) return false;
         if (model.getStone(1, 1) == Stone.X &&
@@ -112,6 +130,12 @@ public class SimpleAI {
         return false;
     }
 
+    /**
+     * Checks all rows if two enemy stones are in the same row
+     * @param model model of the current game board
+     * @return  false if there are no two enemy stones in the same row.
+     *          true if there are two enemy stones in the same row and a counter move was made
+     */
     private boolean checkRows(GameBoard model) {
         for (int r = 0; r < 3; r++) {
             int stoneCount = 0;
@@ -130,6 +154,12 @@ public class SimpleAI {
         return false;
     }
 
+    /**
+     * Checks columns if for enemy stones
+     * @param model model of the current game board
+     * @return  false if threre are no two enemy stones in the same column
+     *          true if there are two enemy stones in the same column and a counter move was made
+     */
     private boolean checkCols(GameBoard model) {
         for (int c = 0; c < 3; c++) {
             int stoneCount = 0;
@@ -146,22 +176,41 @@ public class SimpleAI {
         return false;
     }
 
+    /**
+     * Sets a friendly stone in the appropriate position
+     * @param model model of the current game board
+     * @param c column in which the two enemy stones were found
+     */
     private void counterMoveCol(GameBoard model, int c) {
         for (int r = 0; r < 3; r++) {
             if (model.getStone(r, c) == Stone.NONE) model.setStone(r, c, Stone.O);
         }
     }
 
+    /**
+     * Sets a friendly stone in the appropriate position
+     * @param model model of the current game board
+     * @param r row in which the two enemy stones were found
+     */
     private void counterMoveRow(GameBoard model, int r) {
         for (int c = 0; c < 3; c++) {
             if (model.getStone(r, c) == Stone.NONE) model.setStone(r, c, Stone.O);
         }
     }
 
+    /**
+     * generates a random integer number with a range between 0 and 2
+     * @return random int between 0 and 2
+     */
     private int getRandomNumber() {
         return current().nextInt(0, 3);
     }
 
+    /**
+     * generates an even random number (0 or 2)
+     * used to setting a stone in one of the corners
+     * @return random even int (0 or 2)
+     */
     private int getRandomEvenNumber() {
         return current().nextInt(0, 2) * 2;
     }
